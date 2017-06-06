56ca4b9ac383bd29c003f71ed2718ff459b1f5ae
Joshua Flack <joshu.alexander@gmail.com>
2017-06-06 13:51:46 -0400

    madlib function

---
+++ madlib.js
@@ -0,0 +1,4 @@
+function madlib (noun, verb, adjective, noun2, adverb){
+  var sentence = ("The " + noun + "decided to " + verb + "today. The " + noun + "had " + adjective + noun2 + "s, which helped them " + verb + adverb);
+  return sentence;
+}
